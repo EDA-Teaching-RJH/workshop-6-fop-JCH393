5e2130fc7d36389b9dd1c2806f69229c57ce530f
JCH393 <jch11@kent.ac.uk>
2026-02-12 09:35:09 +0000

    workshop 6

--- challenge.py
+++ challenge.py
@@ -1,1 +1,24 @@
+rover_state = {
+    'x':0,
+    'y':0,
+    'samples':0
+}
+cmds = input('Are there more commands in command_batch? ').strip().capitalize()
+
+if cmds == 'Yes':
+    next_command = input('Move or Dig? ').upper()
+    if next_command == 'MOVE':
+        try:
+            x = input('Distance? ')
+            y = int(x)
+            rover_state['y'] += y
+        except ValueError:
+            print('Bad distance')
+    elif next_command == 'DIG':
+        rover_state['samples'] += 1
+    else:
+        print('Unkown command')
+else:
+    print(rover_state)
+
 
